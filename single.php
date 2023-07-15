1cf54c082e55420700e3833256e8803489cbb9b0
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-07-15 13:26:54 +0100

    added custom type te_course meta

--- single.php
+++ single.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
+<div class="show_page_name">single.php</div>
 
 <?php while ( have_posts() ) : the_post(); ?>
+
 
 <section class="front_page cover_block bg_navy fade_in">
       <?php if(has_post_thumbnail()):?>
@@ -9,12 +11,19 @@
       <?php endif;?>
       <div class="overlay">
          <h2><?php the_title(); ?></h2>
-         <p><?php echo get_post_meta( get_the_ID(), 'te_casestudy_tagline', true ); ?></p>
+         <p><?php echo get_post_meta( get_the_ID(), 'te_course_teacher', true ); ?></p>
       </div>
 </section>
 
 <?php the_content();?>
 
+
+<section>
+   teacher : 
+         <p><?php echo get_post_meta( get_the_ID(), 'te_course_teacher', true ); ?></p>
+   topics :
+         <p><?php echo get_post_meta( get_the_ID(), 'te_course_topics', true ); ?></p>
+</section>
 <?php endwhile; ?>
 
    <div style="display:flex;gap:2rem;">
