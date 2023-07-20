dda55d618fe53a6c0285419a221a56cea3160155
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2023-07-20 14:13:00 +0100

    access job details meta key

--- single-te_job.php
+++ single-te_job.php
@@ -2,12 +2,20 @@
 
 <div class="show_page_name">single-te_job.php</div>
 
-   <?php while ( have_posts() ) : the_post(); ?>
+   <?php 
+   
+   
+   $details= (array) get_post_meta( get_the_ID(),'_te_job_details_meta_key', true );
+
+   while ( have_posts() ) : the_post(); ?>
  
       <h1><?php the_title(); ?></h1>
       <?php if(has_post_thumbnail()):?>
          <img src="<?php the_post_thumbnail_url('medium'); ?>"/>
       <?php endif;?>
+                     
+      <?php echo isset($details['school_or_section']) ? $details['school_or_section'] : '';?>
+
       <?php the_content();?>
 
 
