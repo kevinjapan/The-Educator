3e7f7205cf6af81f1eeef9011b549a588320314f
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-03-08 13:26:20 +0000

    tidy copyright UI

--- footer.php
+++ footer.php
@@ -20,8 +20,20 @@
    </ul>
 
    <ul class="footnote">
-      <li>
-         <div class="footer_copyright"><?php echo esc_html(get_theme_mod('te_copyright'));?></div>
+      <li class="flex fit_content">
+         <div class="footer_copyright fit_content">
+            <?php 
+            echo esc_html(get_theme_mod('te_copyright'));
+            ?>
+         </div>
+         <div class="footer_copyright_auto_complete fit_content">
+            <?php
+            $inc_auto_complete = get_theme_mod('te_copyright_auto_complete');
+            if($inc_auto_complete) {
+               echo esc_html(' - ' . date("Y"));
+            }
+            ?>
+         </div>
       </li>
    </ul>
 
