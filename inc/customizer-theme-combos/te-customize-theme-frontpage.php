986deaa27b0f84768b8690f78bb2dcf98a06ef70
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-03-11 13:03:30 +0000

    Update te-customize-theme-frontpage.php

--- inc/customizer-theme-combos/te-customize-theme-frontpage.php
+++ inc/customizer-theme-combos/te-customize-theme-frontpage.php
@@ -4,30 +4,6 @@
 //
 function te_customize_theme_frontpage($wp_customize) {
 
-   /*
-      * Header Images 
-      * frontpage uses 'Custom Header Image' while other pages override w/ their own 'Featured Image' if present
-      */
-
-   /* FrontPage Header Image Is Fixed */
-   // $wp_customize->add_setting( 'te_is_header_img_fixed',
-   // array('default'    => false, 
-   //       'type'       => 'theme_mod',
-   //       'capability' => 'edit_theme_options',
-   //       'transport'  => 'postMessage',
-   //       'sanitize_callback' => 'te_sanitize_checkbox') 
-   // );
-
-   // $wp_customize->add_control( 'te_is_header_img_fixed', 
-   //    array('type' => 'checkbox',
-   //          'priority' => 10,
-   //          'section' => 'te_header_img',
-   //          'settings'   => 'te_is_header_img_fixed', 
-   //          'input_attrs' => array( 'style' => 'width:100%;' ),
-   //          'description' => esc_html__( 'Header images are in fixed positions.','the-educator'))
-   // );
-
-   /* FrontPage Header Height */
    $wp_customize->add_setting( 'frontpage_header_height', 
       array('default'    => '100', 
             'type'       => 'theme_mod', 
@@ -44,8 +20,6 @@ function te_customize_theme_frontpage($wp_customize) {
             'description' => esc_html__( 'set the % height of the header image','the-educator'),
             'input_attrs' => array( 'min' => 20, 'max' => 100, 'style' => 'width: 80px;', 'step'	=> 5 )) 
    );
-
-   /* Site Header BG Color - used in sm viewports / rqrd for hero text contrast */
    $wp_customize->add_setting( 'frontpage_header_bg_color',
       array('default'    => '#000080',
             'type'       => 'theme_mod',
