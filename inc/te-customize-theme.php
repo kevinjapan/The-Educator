43d4f9730875505aabde347b14a361ea0526b7a3
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-03-13 13:02:19 +0000

    Update te-customize-theme.php

--- inc/te-customize-theme.php
+++ inc/te-customize-theme.php
@@ -33,16 +33,14 @@ class TheEducatorThemeCustomizer {
             $wp_customize->add_panel(
                'te_layout_panel',
                array(
-                  'priority' => 25,
-                  'title' => esc_html__( 'The Educator','the-educator'),
+                  'priority' => 60,
+                  'title' => esc_html__( 'The Educator Theme','the-educator'),
                   'description' => esc_html__('Customize your The Educator theme here.', 'the-educator'))
             );
          }
       }
 
-      // to do : review all prev. sections / combos 
 
-      //
       // Theme Customizer Sections
       //
       $wp_customize->add_section( 'te_typography', 
@@ -54,21 +52,6 @@ class TheEducatorThemeCustomizer {
                'active_callback' => ''
          ) 
       );
-      // $wp_customize->add_section( 'te_nav', 
-      //    array('title'       => esc_html( 'Top Navigation', 'the-educator' ),
-      //          'priority'    => 20,
-      //          'capability'  => 'edit_theme_options',
-      //          'description' => esc_html('You can customize the top navigation menu here.', 'the-educator'),
-      //          'panel' => 'te_layout_panel'
-      //    ) 
-      // );
-      // $wp_customize->add_section( 'te_header', 
-      //    array('title'       => esc_html( 'Title and Tagline', 'the-educator' ),
-      //          'priority'    => 30,
-      //          'capability'  => 'edit_theme_options',
-      //          'description' => esc_html('You can customize the display of your Site Identity here.', 'the-educator'),
-      //          'panel' => 'te_layout_panel') 
-      // );
       $wp_customize->add_section( 'te_header_img', 
          array('title'       => esc_html( 'FrontPage Header Image', 'the-educator' ),
                'priority'    => 40,
@@ -77,30 +60,6 @@ class TheEducatorThemeCustomizer {
                'panel' => 'te_layout_panel',
                'active_callback' => 'is_front_page',) 
       );
-      // $wp_customize->add_section( 'te_page_header_img', 
-      //    array('title'       => esc_html( 'Pages Header Image', 'the-educator' ),
-      //          'priority'    => 40,
-      //          'capability'  => 'edit_theme_options',
-      //          'description' => esc_html('You can modify some aspects of the page Header Image here for all pages other than the frontpage. To change the individual images themselves, select a \'Featured Image\' for each page in the Page Editor. If no Featured Image is selected, pages will default to the site \'Header Image\' as used by the frontpage.', 'the-educator'),
-      //          'panel' => 'te_layout_panel',
-      //          'active_callback' => 'te_is_general_page') 
-      // );
-      // $wp_customize->add_section( 'te_blog_header_img', 
-      //    array('title'       => esc_html( 'Blog Header Image', 'the-educator' ),
-      //          'priority'    => 40,
-      //          'capability'  => 'edit_theme_options',
-      //          'description' => esc_html__('The Educator Blog Header Image customizations.', 'the-educator'),
-      //          'panel' => 'te_layout_panel',
-      //          'active_callback' => 'te_is_blog_archive_page') 
-      // );
-      // $wp_customize->add_section( 'te_hero', 
-      //    array('title'       => esc_html__( 'Hero Text', 'the-educator' ),
-      //          'priority'    => 50,
-      //          'capability'  => 'edit_theme_options',
-      //          'description' => esc_html__('You can customize a hero text to overlay your frontpage header image here.', 'the-educator'),
-      //          'panel' => 'te_layout_panel',
-      //          'active_callback' => 'is_front_page',) 
-      // );
       $wp_customize->add_section( 'te_posts', 
          array('title'       => esc_html__( 'Posts', 'the-educator' ),
                'priority'    => 52,
@@ -117,13 +76,10 @@ class TheEducatorThemeCustomizer {
       );
       
 
-
       //
       // Add settings and controls inside the theme panel sections
       //
-
       
-      //
       // Theme Sections Settings/Controls combos
       //
       te_customize_theme_copyright($wp_customize);
@@ -132,63 +88,8 @@ class TheEducatorThemeCustomizer {
       te_customize_theme_posts($wp_customize);
 
 
-
-      /*
-      // Top Navigation
-       */
-      // $wp_customize->add_setting( 'te_is_nav_sticky',
-      // array('default'    => false, 
-      //       'type'       => 'theme_mod',
-      //       'capability' => 'edit_theme_options',
-      //       'transport'  => 'postMessage',
-      //       'sanitize_callback' => 'te_sanitize_checkbox') 
-      // );
-      // $wp_customize->add_control( 'te_is_nav_sticky', 
-      //    array('type' => 'checkbox',
-      //          'priority' => 10,
-      //          'section' => 'te_nav',
-      //          'settings'   => 'te_is_nav_sticky', 
-      //          'input_attrs' => array( 'style' => 'width:100%;' ),
-      //          'description' => esc_html__( 'Navigation always visible.','the-educator' ))
-      // );
-      
-
-      /* Nav Text Color */
-      // $wp_customize->add_setting( 'nav_color',
-      //    array('default'    => '#ffffff', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color') 
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'nav_color',
-      //    array('label'      => esc_html__( 'Top Navigation Bar', 'the-educator' ), 
-      //          'description' => esc_html__( 'text color' ,'the-educator'),
-      //          'settings'   => 'nav_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_nav') 
-      // ));
-
-      /* Nav BG Color */
-      // $wp_customize->add_setting('nav_bg_color',
-      //    array('capability' => 'edit_theme_options',
-      //          'type'       => 'theme_mod',
-      //          'default'    => '#000080',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color')
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'nav_bg_color',
-      //    array('description' => esc_html__( 'background color' ,'the-educator'),
-      //          'settings'   => 'nav_bg_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_nav') 
-      // ));
-
-      /* Nav BG Opacity */
+      // Nav BG Opacity 
+      //
       $wp_customize->add_setting( 'nav_bg_opacity',
          array('default'    => '1', 
                'type'       => 'theme_mod',
@@ -205,63 +106,9 @@ class TheEducatorThemeCustomizer {
                'input_attrs' => array( 'min' => 0, 'max' => 1, 'style' => 'width: 80px;', 'step'	=> .1 ))
       ); 
 
-      /* Nav Dropdown Text Color */
-      // $wp_customize->add_setting( 'nav_dropdown_color',
-      //    array('default'    => '#000', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color') 
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'nav_dropdown_color',
-      //    array('label'      => esc_html__( 'Dropdown Sub-menu', 'the-educator' ), 
-      //          'description' => esc_html__( 'text color' ,'the-educator'),
-      //          'settings'   => 'nav_dropdown_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_nav') 
-      // ));
-      
-      /* Nav Dropdown BG Color */
-      // $wp_customize->add_setting('nav_dropdown_bg_color',
-      //    array('capability' => 'edit_theme_options',
-      //          'type'       => 'theme_mod',
-      //          'default'    => '#fff',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color')
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'nav_dropdown_bg_color',
-      //    array('description' => esc_html__( 'background color' ,'the-educator'),
-      //          'settings'   => 'nav_dropdown_bg_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_nav') 
-      // ));
-
-      /* Nav Dropdown BG Opacity */
-      // $wp_customize->add_setting( 'nav_dropdown_bg_opacity',
-      //    array('default'    => '1', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'te_sanitize_number_range') 
-      // );
-      // /*$wp_customize->add_control( 'nav_dropdown_bg_opacity', 
-      //    array('type' => 'number',
-      //          'priority' => 10,
-      //          'section' => 'te_nav',
-      //          'settings'   => 'nav_dropdown_bg_opacity', 
-      //          'description' => esc_html__( 'backround opacity','the-educator' ),
-      //          'input_attrs' => array( 'min' => 0, 'max' => 1, 'style' => 'width: 80px;', 'step'	=> .1 ))
-      // ); */
-
- 
-   
 
       // Hero Text & Layout
-      
+      //
 
       // Hero Text
       $wp_customize->add_setting( 'te_hero_text',
@@ -281,7 +128,9 @@ class TheEducatorThemeCustomizer {
                'input_attrs' => array('style' => 'width: 96%;')) 
       );
       
+
       // Hero Text Color
+      //
       $wp_customize->add_setting( 'te_hero_text_color',
          array('default'    => '#ffffff', 
                'type'       => 'theme_mod',
@@ -333,7 +182,9 @@ class TheEducatorThemeCustomizer {
                'input_attrs' => array( 'min' => 0, 'max' => 1, 'style' => 'width: 80px;', 'step'	=> .1 ))
       );
       
-      /* Hero % screen width */
+
+      // Hero % screen width 
+      //
       $wp_customize->add_setting( 'hero_x_width',
          array('default'    => '100', 
                'type'       => 'theme_mod',
@@ -351,7 +202,8 @@ class TheEducatorThemeCustomizer {
                'input_attrs' => array( 'min' => 20, 'max' => 100, 'style' => 'width: 80px;', 'step'	=> 5 )) 
       );
       
-      /* Hero Text Alignment */
+      // Hero Text Alignment
+      //
       $wp_customize->add_setting( 'te_hero_text_align', 
          array('default' 	        => 'Center',
                'transport'         => 'postMessage',
@@ -370,7 +222,8 @@ class TheEducatorThemeCustomizer {
                'input_attrs' => array( 'style' => 'width: 80px;' )) 
       );
       
-      /* Hero Alignment */
+      // Hero Alignment
+      //
       $wp_customize->add_setting( 'te_hero_align', 
          array('default' 	        => 'Center',
                'transport'         => 'postMessage',
@@ -388,122 +241,7 @@ class TheEducatorThemeCustomizer {
                   'flex-end'  => 'End'),	
          'input_attrs' => array( 'style' => 'width: 80px;' )) 
       );
-      
-
-      /*
-       * Title & Tagline Text & Layout
-       */
-
-      /* Title Text Color */
-      // $wp_customize->add_setting( 'site_title_color',
-      //    array('default'    => '#ffffff', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color') 
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'site_title_color',
-      //    array('label'      => esc_html__( 'Title and Tagline Text Color', 'the-educator' ), 
-      //          'settings'   => 'site_title_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_header') 
-      // ));
-      
-      /* Title BG Color */
-      // $wp_customize->add_setting( 'site_title_bg_color',
-      //    array('default'    => '#000080',
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color') 
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'site_title_bg_color',
-      //    array('label'      => esc_html__( 'Title', 'the-educator' ), 
-      //          'description' => esc_html__( 'background color','the-educator'),
-      //          'settings'   => 'site_title_bg_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_header') 
-      // ));
-      
-      /* Title BG Opacity */
-      // $wp_customize->add_setting( 'site_title_bg_opacity',
-      //    array('default'    => '.5', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'te_sanitize_number_range') 
-      // );
-      // $wp_customize->add_control( 'site_title_bg_opacity', 
-      //    array('type' => 'number',
-      //          'priority' => 10,
-      //          'section' => 'te_header',
-      //          'settings'   => 'site_title_bg_opacity', 
-      //          'description' => esc_html__( 'background opacity','the-educator'),
-      //          'input_attrs' => array( 'min' => 0, 'max' => 1, 'style' => 'width: 80px;', 'step'	=> .1 )) 
-      // );
-
-      /* Tagline BG Color */
-      // $wp_customize->add_setting( 'site_tagline_bg_color',
-      //    array('default'    => '#000080',
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'sanitize_hex_color') 
-      // );
-      // $wp_customize->add_control( new WP_Customize_Color_Control(
-      //    $wp_customize, 
-      //    'site_tagline_bg_color',
-      //    array('label'      => esc_html__( 'Tagline', 'the-educator' ),
-      //          'description' => esc_html__( 'background color','the-educator'), 
-      //          'settings'   => 'site_tagline_bg_color', 
-      //          'priority'   => 10,
-      //          'section'    => 'te_header') 
-      // ));
-      
-      /* Tagline BG Opacity */
-      // $wp_customize->add_setting( 'site_tagline_bg_opacity',
-      //    array('default'    => '.5', 
-      //          'type'       => 'theme_mod',
-      //          'capability' => 'edit_theme_options',
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'te_sanitize_number_range') 
-      // );
-      // $wp_customize->add_control( 'site_tagline_bg_opacity', 
-      //    array('type' => 'number',
-      //          'priority' => 10,
-      //          'section' => 'te_header',
-      //          'settings'   => 'site_tagline_bg_opacity', 
-      //          'description' => esc_html__( 'background opacity','the-educator'),
-      //          'input_attrs' => array( 'min' => 0, 'max' => 1, 'style' => 'width: 80px;', 'step'	=> .1 )) 
-      // );
-
-
-
-      
-      /* Header Height */
-      // $wp_customize->add_setting( 'header_height', 
-      //    array('default'    => '60', 
-      //          'type'       => 'theme_mod', 
-      //          'capability' => 'edit_theme_options', 
-      //          'transport'  => 'postMessage',
-      //          'sanitize_callback' => 'te_sanitize_number_range'
-      //    ) 
-      // );
-      // $wp_customize->add_control( 'te_header_height', 
-      //    array('type' => 'number',
-      //          'priority' => 10,
-      //          'section' => 'te_page_header_img',
-      //          'label' => esc_html__( 'Image Height','the-educator'),
-      //          'settings'   => 'header_height', 
-      //          'description' => esc_html__( 'set the % height of the header image','the-educator'),
-      //          'input_attrs' => array( 'min' => 20, 'max' => 100, 'style' => 'width: 80px;', 'step'	=> 5))
-      // );
-
-
+   
    }
 
 
@@ -528,13 +266,11 @@ class TheEducatorThemeCustomizer {
 }
 
 
-//
 // setup theme customizer settings and controls
 //
 add_action( 'customize_register', array( 'TheEducatorThemeCustomizer' , 'register' ) );
 
 
-//
 // output custom css to frontend
 //
 add_action('wp_head', array( 'TheEducatorThemeCustomizer' , 'te_customizer_theme_styles' ) );
